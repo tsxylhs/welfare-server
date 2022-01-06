7d4fe21fd2b9a9bd7c37fad57e9190c35336d3f4
lvhoushuai_vendor <lvhoushuai_vendor@sensetime.com>
2022-01-06 13:50:35 +0800

    add test

--- reqUtils/lotteryReq_test.go
+++ reqUtils/lotteryReq_test.go
@@ -1,8 +1,18 @@
 package reqUtils
 
-import "testing"
+import (
+	"log"
+	"lottery/welfare/model"
+	"testing"
+)
 
 func Test_lotteryTypes(t *testing.T) {
 	LotteryReq.LotteryTypes()
+	t.Log()
+}
+func Test_lotteryBouns(t *testing.T) {
+	selectlottery := &model.SelectLottery{}
+	res := LotteryReq.LotteryBonus(selectlottery)
+	log.Fatal(res.LotteryPrize...)
 	t.Log()
 }
